9f7b9091f9e6f0a0479f3e01db09b22a3063818a
ohgus <anakin1108@naver.com>
2023-10-06 16:10:40 +0900

    fix logo default theme

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,6 +4,6 @@ import { ThemeProvider } from "next-themes";
 
 export function Providers({children}: {children: React.ReactNode}) {
     return (
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>{children}</ThemeProvider>
+        <ThemeProvider attribute="class" enableSystem={false}>{children}</ThemeProvider>
     )
 }
