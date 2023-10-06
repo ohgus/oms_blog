9722dea8410602e805df459a501e24dfe917ebba
ohgus <anakin1108@naver.com>
2023-10-06 14:34:40 +0900

    logo size fix & modeToggle location fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,14 +26,17 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <Providers>
           <div className="max-w-2xl mx-auto py-10 px-4">
             <header>
+              <div className="flex items-center justify-between">
               <CabinLogo />
+              <ModeToggle />
+              </div>
               <p className="mb-3">계속 미래를 그려나가는 개발자가 되자.</p>
               <div className="flex items-center justify-between">
                 <nav className="ml-0 mr-4 text-m font-medium space-x-6">
-                  <Link href="/about">About</Link>
+                  <Link href="/algorithm">Algorithm</Link>
                   <Link href="/portfolio">Portfolio</Link>
+                  <Link href="/about">About</Link>
                 </nav>
-                <ModeToggle />
               </div>
             </header>
             <main>{children}</main>
