2f847a0cc1e96c57f929904c547d324107543c2d
ohgus <anakin1108@naver.com>
2023-10-11 20:35:15 +0900

    next config images setup

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const { withContentlayer } = require("next-contentlayer");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 60,
+  }
 };
 
 module.exports = withContentlayer(nextConfig);
