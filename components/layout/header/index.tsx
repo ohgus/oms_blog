5541917cce867d506e5429ed5798d06fcfd4853a
ohgus <anakin1108@naver.com>
2023-10-11 19:39:51 +0900

    navbar 컴포넌트 분리

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -1,22 +1,16 @@
 import CabinLogo from "@/components/icons/cabinLogo";
 import { ModeToggle } from "@/components/mode-toggle";
-import Link from "next/link";
+import Navbar from "../navbar";
 
 export default function Header() {
     return (
-        <header>
-              <div className="flex items-center justify-between">
-                <CabinLogo />   
-                <ModeToggle />
-              </div>
-              <p className="mb-3 font-semibold">계속 미래를 그려나가는 개발자가 되자.</p>
-              <div className="flex items-center justify-between">
-                <nav className="ml-0 mr-4 text-lg font-medium space-x-6">
-                  <Link className="font-semibold" href="/algorithm">Algorithm</Link>
-                  <Link className="font-semibold" href="/portfolio">Portfolio</Link>
-                  <Link className="font-bold" href="/about">About</Link>
-                </nav>
-                </div>
-            </header>
+        <header className="sticky top-0">
+            <div className="flex items-center justify-between">
+              <CabinLogo />   
+              <ModeToggle />
+            </div>
+            <p className="mb-3 font-semibold">성장을 기록하는 블로그 입니다.</p>
+            <Navbar />   
+        </header>
     )
 }
