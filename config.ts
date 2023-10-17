3407772dfaf73285390b397be14ec22d2dfff891
ohgus <anakin1108@naver.com>
2023-10-17 18:41:35 +0900

    make siteconfig

---
+++ config.ts
@@ -0,0 +1,22 @@
+export const SiteConfig = {
+    url: "https://ohgus.dev",
+    pathPrefix: "/",
+    title: "ohgus",
+    subtitle: "ohgus",
+    copyright: "ohgus © All rights reserved.",
+    postPerPage: 5,
+    menu: [
+        {
+            label: "Posts",
+            path: "/pages/1",
+        },
+        {
+            label: "Tags",
+            path: "/tags",
+        },
+        {
+            label: "about",
+            path: "/about",
+        },
+    ],
+}
