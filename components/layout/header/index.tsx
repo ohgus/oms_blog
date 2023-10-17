1d958d014db7810457ffe40c56cf6c77ce352f03
ohgus <anakin1108@naver.com>
2023-10-17 16:53:26 +0900

    header layout change

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -1,11 +1,9 @@
-import CabinLogo from "@/components/icons/cabinLogo";
-import { ModeToggle } from "@/components/mode-toggle";
+import Navbar from "../navbar"
 
 export default function Header() {
     return (
-        <header className="sticky px-2 py-3 z-20 bg-orange-50 dark:bg-stone-800 top-0">
-            <div className="flex items-center justify-between">
-            </div>
+        <header className="sticky px-2 z-20 bg-orange-50 dark:bg-stone-800 top-0">
+            <Navbar />
         </header>
     )
 }
--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,19 +1,31 @@
-import AlgorithmLogo from "@/components/icons/algorithmLogo";
 import PortfolioLogo from "@/components/icons/portfolioLogo";
-import StudyLogo from "@/components/icons/studyLogo";
 import AboutLogo from "@/components/icons/aboutLogo";
 import { ModeToggle } from "@/components/mode-toggle";
 import CabinLogo from "@/components/icons/cabinLogo";
+import Link from "next/link";
+import Image from "next/image";
+import home from "@/public/home.png";
 
 export default function Navbar() {
     return(
-        <div className="flex items-center justify-between">
-            <nav className="flex items-center justify-between ml-2">
-                <div className="flex items-center justify-between">
-                    <a href="/">
-                        
-                        <CabinLogo />
-                    </a>
+        <div className="flex flex-col justify-between">
+            <nav className="flex items-center justify-between py-10">
+                <div>
+                    <Link href="/">
+                        <div className="flex items-center justify-between">
+                            <div className="mr-3">
+                                <Image
+                                    src={home}
+                                    alt="home"
+                                    width={40}
+                                    height={40}
+                                    placeholder="blur"
+                                    className="h-10 w-10 rounded-full"
+                                    />
+                            </div>
+                            <CabinLogo />
+                        </div>
+                    </Link>
                 </div>
                 <div className="flex items-center justify-end gap-x-3">
                     <PortfolioLogo />
