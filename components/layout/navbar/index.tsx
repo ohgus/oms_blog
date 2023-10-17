fd4a03dcca3aa692e519d39307ff1e7a8e8ca2ad
ohgus <anakin1108@naver.com>
2023-10-17 19:49:03 +0900

    add mobileNav & changes another items position

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,10 +1,13 @@
-import PortfolioLogo from "@/components/icons/portfolioLogo";
-import AboutLogo from "@/components/icons/aboutLogo";
-import { ModeToggle } from "@/components/mode-toggle";
-import CabinLogo from "@/components/icons/cabinLogo";
 import Link from "next/link";
 import Image from "next/image";
+
+import { ModeToggle } from "@/components/mode-toggle";
+import PostsLogo from "@/components/icons/postsLogo";
+import AboutLogo from "@/components/icons/aboutLogo";
+import CabinLogo from "@/components/icons/cabinLogo";
+import TagsLogo from "@/components/icons/tagsLogo";
 import home from "@/public/home.png";
+import MobileNav from "../mobileNav";
 
 export default function Navbar() {
     return(
@@ -12,7 +15,7 @@ export default function Navbar() {
             <nav className="flex items-center justify-between py-10">
                 <div>
                     <Link href="/">
-                        <div className="flex items-center justify-between">
+                        <div className="flex items-center justify-between pb-3">
                             <div className="mr-3">
                                 <Image
                                     src={home}
@@ -23,14 +26,22 @@ export default function Navbar() {
                                     className="h-10 w-10 rounded-full"
                                     />
                             </div>
-                            <CabinLogo />
+                            <div className="hidden h-6 sm:block">
+                                <CabinLogo />
+                            </div>
                         </div>
                     </Link>
                 </div>
-                <div className="flex items-center justify-end gap-x-3">
-                    <PortfolioLogo />
-                    <AboutLogo />
+                <div className="flex items-center">
+                    <div className="hidden sm:block">
+                        <div className="flex items-center justify-between gap-y-1 mr-2">
+                            <PostsLogo />
+                            <TagsLogo />
+                            <AboutLogo />
+                        </div>
+                    </div>
                     <ModeToggle />
+                    <MobileNav />
                 </div>
             </nav>
         </div>
