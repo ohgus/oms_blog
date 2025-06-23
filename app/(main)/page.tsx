71da95de363c37bbe31e228d122e3b40764e01b5
ohgus <anakin1108@naver.com>
2025-06-23 20:45:22 +0900

    refactor: 최신 5개의 포스트만 보여주게 수정

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,10 +1,17 @@
-import { allPosts } from "@/.contentlayer/generated"
-import PostCard from "@/components/post-card";
+import { allPosts } from '@/.contentlayer/generated';
+import PostCard from '@/components/post-card';
 
 export default async function Home() {
+  const mainPosts = allPosts
+    .sort(
+      (a, b) =>
+        new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime()
+    )
+    .slice(0, 5);
+
   return (
     <div>
-      {allPosts.map((post) => (
+      {mainPosts.map((post) => (
         <PostCard post={post} key={post._id} />
       ))}
     </div>
