4d10ce410fc4c3ab93639d2acad9faa39dc0407a
ohgus <anakin1108@naver.com>
2025-11-04 14:30:30 +0900

    chore: 코드블럭 강조를 위한 플러그인 추가

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,54 +1,77 @@
-import { defineDocumentType, makeSource } from "contentlayer/source-files"
-import { getSlugWithoutCategoryPath } from "./libs/mdx";
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+import rehypePrettyCode from 'rehype-pretty-code';
+import { getSlugWithoutCategoryPath } from './libs/mdx';
 
 /** @type {import('contentlayer/source-files').ComputedFields} */
 const computedFields = {
   slug: {
-    type: "string",
+    type: 'string',
     resolve: (doc) => getSlugWithoutCategoryPath(doc),
   },
   url_path: {
-    type: "string",
+    type: 'string',
     resolve: (doc) => `/post/${getSlugWithoutCategoryPath(doc)}`,
   },
 };
 
 export const Post = defineDocumentType(() => ({
-  name: "Post",
+  name: 'Post',
   filePathPattern: `**/*.mdx`,
-  contentType: "mdx",
+  contentType: 'mdx',
   fields: {
     title: {
-      type: "string",
+      type: 'string',
       required: true,
     },
     publishedAt: {
-      type: "string",
+      type: 'string',
       required: true,
     },
     category: {
-      type: "string",
+      type: 'string',
       required: true,
     },
     image: {
-      type: "string",
-      description: "The image with local path. Use for SEO, thumbnail, etc.",
+      type: 'string',
+      description: 'The image with local path. Use for SEO, thumbnail, etc.',
     },
     description: {
-      type: "string",
+      type: 'string',
     },
     tags: {
-      type: "list",
-      of: { type: "string" },
+      type: 'list',
+      of: { type: 'string' },
     },
     series: {
-      type: "string",
+      type: 'string',
     },
   },
   computedFields,
 }));
 
 export default makeSource({
-  contentDirPath: "./posts",
+  contentDirPath: './posts',
   documentTypes: [Post],
+  mdx: {
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          onVisitLine(node) {
+            // Prevent lines from collapsing in `display: grid` mode, and allow empty
+            // lines to be copyable
+            if (node.children.length === 0) {
+              node.children = [{ type: 'text', value: ' ' }];
+            }
+          },
+          onVisitHighlightedLine(node) {
+            node.properties.className = ['line--highlighted'];
+          },
+          onVisitHighlightedWord(node) {
+            node.properties.className = ['word--highlighted'];
+          },
+        },
+      ],
+    ],
+  },
 });
